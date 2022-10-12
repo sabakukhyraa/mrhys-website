0b8d79c556a62f0f19f51e7eb38f54b68d50b793
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2022-10-12 19:23:05 +0300

    new configurations and new pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     "./index.html",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "992px",
+      xl: "1080px",
+      "2xl": "1294px",
+    },
     extend: {
       container: {
         center: true,
@@ -12,11 +19,41 @@ module.exports = {
           DEFAULT: "1rem",
         },
       },
+      colors: {
+        primary: {
+          DEFAULT: "#2191FB",
+          100: "#E8F3FF",
+          200: "#DCEAF8",
+          300: "#AFC1CC",
+          500: "#2191FB",
+          700: "#1066B8",
+          900: "#0E1C36",
+        },
+        secondary: {
+          DEFAULT: "#FB6107",
+          100: "#F9F4F2",
+          200: "#F2E6E0",
+          300: "#F89E6A",
+          500: "#FB6107",
+          700: "#AE4304",
+        },
+        green: {
+          DEFAULT: "#03CEA4",
+          100: "#F6F8F9",
+          200: "#D9F1EF",
+          300: "#9DEEDD",
+          500: "#03CEA4",
+          700: "#079E7F",
+        },
+        black: {
+          700: "#565758",
+        },
+        gray: {},
+        semiTransparent: {
+          DEFAULT: "#ffffffcc",
+        },
+      },
     },
-    colors: {
-      primary: '#5c6ac4',
-      secondary: '#ecc94b',
-    }
   },
   plugins: [],
 }
