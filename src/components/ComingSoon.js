b542a44df5bf02bd66042cb2d0bae8d7e4a3d91a
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2023-01-25 19:12:56 +0300

    ComingSoon refactored

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,13 +1,14 @@
 export default function comingSoon() {
-    return(
-        <section className="bg-amber-500 min-h-screen flex flex-col justify-center items-center gap-12 md:gap-20">
-            <img
-            className="w-64 h-auto md:w-96 animation hover:scale-105" src="/cover.png" alt="MRHYA Logo"
-            />
-
-            <h1>
-                <li className="list-none text-4xl md:text-7xl font-bold text-amber-50">Coming Soon!</li>
-            </h1>
-        </section>
-    )
+  return (
+    <section className="flex flex-col items-center justify-center min-h-screen gap-12 bg-amber-500 md:gap-20">
+      <img
+        className="absolute top-0 w-64 h-auto md:w-96 animation hover:scale-105"
+        src="/cover.png"
+        alt="MRHYA Logo"
+      />
+      <h1 className="mb-5 text-4xl font-bold md:text-7xl text-amber-50">
+        Coming Soon!
+      </h1>
+    </section>
+  );
 }
