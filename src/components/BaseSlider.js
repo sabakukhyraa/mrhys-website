09b59e85f18c53609ab41c283fce5e0646436dee
Ali Kerem Ata <96057812+sabakukhyraa@users.noreply.github.com>
2023-02-02 16:48:28 +0300

    base slider refactored

--- src/components/BaseSlider.js
+++ src/components/BaseSlider.js
@@ -17,21 +17,21 @@ export default function BaseSlider() {
         disableOnInteraction: false,
       }}
     >
-      <SwiperSlide className="!bg-slide-1">
-        <h1>En kaliteli ve en uygun giysiler MRHYA.com'da!</h1>
+      <SwiperSlide className="!bg-slide-1 shadow-3xl">
+        <h1 className="!text-[72px]">En kaliteli ve en uygun giysiler MRHYA.com'da!</h1>
       </SwiperSlide>
-      <SwiperSlide className="!bg-slide-2">
-        <h2>Daha fazla bilgi için bizimle iletişime geçin!</h2>
-        <p>
+      <SwiperSlide className="!bg-slide-2 shadow-3xl">
+        <h2 className="!text-[72px]">Daha fazla bilgi için bizimle iletişime geçin!</h2>
+        <p className="text-2xl">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit.
           Perspiciatis, praesentium sequi! Ipsum id natus deleniti earum, quis
           error? Dolore repellendus cupiditate aliquam debitis maxime nisi quae
           rerum dolorem consequuntur! Veniam.
         </p>
       </SwiperSlide>
-      <SwiperSlide className="!bg-slide-3">
-        <h2>Doğru adrestediniz!</h2>
-        <p>
+      <SwiperSlide className="!bg-slide-3 shadow-3xl">
+        <h2 className="!text-[72px]">Doğru adrestediniz!</h2>
+        <p className="text-2xl">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit.
           Perspiciatis, praesentium sequi! Ipsum id natus deleniti earum, quis
           error? Dolore repellendus cupiditate aliquam debitis maxime nisi quae
